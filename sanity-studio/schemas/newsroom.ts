63e4af2c2b4a7ab38f9d27a42d68f3dde4f0ca2d
iamRahul21 <rahuldeka072@gmail.com>
2026-01-12 12:00:26 +0530

    new logo field added for newsroom

--- sanity-studio/schemas/newsroom.ts
+++ sanity-studio/schemas/newsroom.ts
@@ -49,5 +49,12 @@ export default defineType({
       },
       validation: Rule => Rule.required(),
     }),
+    defineField({
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+      options: { hotspot: true },
+      // Not required
+    }),
   ],
 })
